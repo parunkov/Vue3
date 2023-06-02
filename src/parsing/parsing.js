401f4dc9764faa35552e6a0f8a035fbfe17504c4
parunkov <parunkov@gmail.com>
2023-06-02 08:13:44 +0500

    parsing

--- src/parsing/parsing.js
+++ src/parsing/parsing.js
@@ -1,10 +1,10 @@
 const request = require('request');
-// const fs = require('fs');
-// const DomParser = require('dom-parser');
+const fs = require('fs');
 const jsdom = require('jsdom');
 
 const { JSDOM } = jsdom;
 
+const data = [];
 const parseMenu = (url) => {
   request(url, (err, res, body) => {
     if (err) throw err;
@@ -12,31 +12,27 @@ const parseMenu = (url) => {
     const dom = new JSDOM(body);
     const els = dom.window.document.querySelectorAll('.catalog-main__item');
     els.forEach((item) => {
-      console.log(item.querySelector('.catalog-main__item-name').textContent);
+      const id = item.dataset.productId;
+      const title = item.querySelector('.catalog-main__item-name').textContent;
+      const image = 'https://kz.iqos.com' + item.querySelector('.catalog-main__item-img').src;
+      const icon = item.querySelector('.catalog-main__item-aromatic-icon')
+        ? 'https://kz.iqos.com' + item.querySelector('.catalog-main__item-aromatic-icon').src
+        : null;
+      const text = item.querySelector('.catalog-main__item-aromatic-text')
+        ? item.querySelector('.catalog-main__item-aromatic-text').textContent
+        : null;
+      const textColor = item.querySelector('.catalog-main__item-aromatic-text')
+        ? item.querySelector('.catalog-main__item-aromatic-text').style._values.color
+        : null;
+      const price = item.querySelector('.catalog-main__item-price').textContent;
+      const buttonText = item.querySelector('a.btn.black').textContent;
+      data.push({ id, title, image, icon, text, textColor, price, buttonText });
     });
-
-    // console.log(body.split('<img class="catalog-main__item-img"').length);
-    // const dom = parser.parseFromString(body);
-    // console.log(dom.getElementsByClassName('catalog-main__item'));
-    // console.log(dom.getElementById(''));
-    // const subcategoriesStart = body.split('<div class="subcategories"')[1];
-    // if (subcategoriesStart) {
-    //   const links = subcategoriesStart.split('<a href="');
-    //   links.forEach(async (item) => {
-    //     const link = item.split('"')[0];
-    //     const titleStart = item.split('div class="subcategories--title"')[1];
-    //     if (titleStart) {
-    //       const title = titleStart.split('>')[1].split('<')[0].split('\n')[0];
-    //       const categoryData = { title, link, menu: [] };
-    //       subcategory.menu.push(categoryData);
-    //       parseMenu(link, categoryData);
-    //       console.log(data);
-    //       await fs.writeFile(`./data/menu.json`, JSON.stringify(data), (error) => {
-    //         if (error) throw error;
-    //       });
-    //     }
-    //   });
-    // }
+    console.log(data.length);
+    fs.writeFile(`./catalogData.json`, JSON.stringify(data), (error) => {
+      if (error) throw error;
+    });
   });
 };
+
 parseMenu('https://kz.iqos.com/ru/catalog/#');
